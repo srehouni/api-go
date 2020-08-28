58a163aaa139d823814c2db388669510a770feeb
Said Rehouni <said_rehouni@hotmail.com>
2020-08-29 00:22:30 +0200

    Created GopherRepository interface

--- pkg/gopher.go
+++ pkg/gopher.go
@@ -6,3 +6,17 @@ type Gopher struct {
 	Image string `json:"image,omitempty"`
 	Age   int    `json:"age,omitempty"`
 }
+
+// Repository provides access to the gopher storage
+type GopherRepository interface {
+	// CreateGopher saves a given gopher
+	CreateGopher(g *Gopher) error
+	// FetchGophers return all gophers saved in storage
+	FetchGophers() ([]*Gopher, error)
+	// DeleteGopher remove gopher with given ID
+	DeleteGopher(ID string) error
+	// UpdateGopher modify gopher with given ID and given new data
+	UpdateGopher(ID string, g *Gopher) error
+	// FetchGopherByID returns the gopher with given ID
+	FetchGopherByID(ID string) (*Gopher, error)
+}
