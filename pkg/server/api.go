f244bff0e54071650732248b7f2e0d3e18a9e492
Said Rehouni <said.rehouni@gmail.com>
2020-08-29 00:06:32 +0200

    Added basic routing to api class

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -18,6 +18,10 @@ func New() Server {
 	a := &api{}
 
 	r := mux.NewRouter()
+
+	r.HandleFunc("/gophers", a.fetchGophers).Methods(http.MethodGet)
+	r.HandleFunc("/gophers/{ID:[a-zA-Z0-9_]+}", a.fetchGopher).Methods(http.MethodGet)
+
 	a.router = r
 	return a
 }
@@ -25,3 +29,7 @@ func New() Server {
 func (a *api) Router() http.Handler {
 	return a.router
 }
+
+func (a *api) fetchGophers(w http.ResponseWriter, r *http.Request) {}
+
+func (a *api) fetchGopher(w http.ResponseWriter, r *http.Request) {}
